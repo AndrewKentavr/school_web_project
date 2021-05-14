360c7f77eaaed7c33ef6ce9ce1129e161f8c7346
andrt <andr.toshachakov@gmail.com>
2021-05-14 07:28:22 +0300

    First commit

---
+++ main_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Исторический турнир</title>
+  <link rel='stylesheet' type='text/css' href='styles.css'>
+ </head>
+
+ <body link='white'>
+  <div id="container">
+
+   <div id="header">Библиотека котов</div>
+
+   <div id="sidebar">
+
+    <p><a href="stat.html">О нас</a></p>
+    <p><a href="interview.html">Таблица с авторами</a></p>
+    <p><a href="ask.html">Выход из аккаунта</a></p>
+   </div>
+
+   <div id="content">
+
+      <button onclick="location.href = 'pyshkin.php';" id='btn'>Пушкин</button><br><br>
+      <br><br>
+      <button onclick="location.href = 'tolstoy.php';" id='btn'>Толстой</button><br><br>
+      <br><br>
+      <button onclick="location.href = 'gogol.php';" id='btn'>Гоголь</button><br><br>
+      <br><br>
+      <button onclick="location.href = 'lermontov.php';" id='btn'>Лермонтов</button><br><br>
+   </div>
+   <div id="footer">&copy; Андрей Тощаков 10 "Б"</div>
+  </div> 
+ </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <form action="signup-check.php" method="post">
+     	<h2>Регестрация</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+          <?php if (isset($_GET['success'])) { ?>
+               <p class="success"><?php echo $_GET['success']; ?></p>
+          <?php } ?>
+
+          <label>Имя пользователя</label>
+          <?php if (isset($_GET['uname'])) { ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="Имя пользователя"
+                      value="<?php echo $_GET['uname']; ?>"><br>
+          <?php }else{ ?>
+               <input type="text" 
+                      name="uname" 
+                      placeholder="Имя пользователя"><br>
+          <?php }?>
+
+
+     	<label>Пароль</label>
+     	<input type="password" 
+                 name="password" 
+                 placeholder="Пароль"><br>
+
+          <label>Подтверждение пароля</label>
+          <input type="password" 
+                 name="re_password" 
+                 placeholder="Подтверждение пароля"><br>
+
+     	<button type="submit">Регистрация</button>
+          <a href="index.php" class="ca">Уже есть аккаунт?</a>
+     </form>
+</body>
+</html>
