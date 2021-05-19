f1da6e2f07daa8108022b8d89392e439897b213f
andrt <andr.toshachakov@gmail.com>
2021-05-19 16:17:02 +0300

    Кароче, это ещё то что я сделал в школе

---
+++ pushkin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Сори</title>
+  <link rel='stylesheet' type='text/css' href='styles.css'>
+ </head>
+
+ <body link='white'>
+  <div id="container">
+
+   <div id="header">Библиотека котов</div>
+
+   <div id="sidebar">
+
+    <p><a href="stat.html">О нас</a></p>
+    <p><a href="interview.html">Таблица с авторами</a></p>
+    <p><a href="ask.html">Выход из аккаунта</a></p>
+   </div>
+
+   <div id="content">
+
+    <ul>
+      <li><a href="books/Пушкин/евгений_онегин.pdf" download>Скачать файл Евгений Онегин</a></li>
+      <li>Крокодил Гена</li>
+      <li>Шапокляк</li>
+    </ul>
+   </div>
+   <div id="footer">&copy; Андрей Тощаков 10 "Б"</div>
+  </div> 
+ </body>
+</html>
